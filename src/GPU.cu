04d744dc1a2eaeded168a267b35cd68ef0c20d66
benlaflen <114254189+benlaflen@users.noreply.github.com>
2026-05-11 13:47:39 -0700

    logging

--- src/GPU.cu
+++ src/GPU.cu
@@ -262,7 +262,7 @@ std::vector<std::vector<HV_16>> encode(const std::vector<int16_t>& kmer_map, con
                         flat.begin() + flat_start + HV_DIM);
                 }
             }
-            int i = i < (int)gpu_win_offsets.size() - 2;
+            int i = (int)gpu_win_offsets.size() - 2;
             int wins = gpu_win_offsets[i + 1] - gpu_win_offsets[i];
             int w = wins-1;
             std::cout << "\nflat_start: " << std::to_string(((size_t)gpu_win_offsets[i] + w) * HV_DIM) << "\n";

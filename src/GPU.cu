33841db06246dfb5f9a1b825ef7a4bc74f5c9ef9
benlaflen <114254189+benlaflen@users.noreply.github.com>
2026-05-12 12:41:11 -0700

    Trying with bigger flat_start because we may be going out of bounds of int32

--- src/GPU.cu
+++ src/GPU.cu
@@ -256,16 +256,14 @@ std::vector<std::vector<HV_16>> encode(const std::vector<int16_t>& kmer_map, con
                 int wins = gpu_win_offsets[i + 1] - gpu_win_offsets[i];
                 output[gpu_batch_start + i].resize(wins);
                 for (int w = 0; w < wins; w++) {
-                    int flat_start = ((size_t)gpu_win_offsets[i] + w) * HV_DIM;
+                    size_t flat_start = ((size_t)gpu_win_offsets[i] + w) * HV_DIM;
                     output[gpu_batch_start + i][w] = HV_16(
                         flat.begin() + flat_start,
                         flat.begin() + flat_start + HV_DIM);
                 }
             }
-            int i = (int)gpu_win_offsets.size() - 2;
-            int wins = gpu_win_offsets[i + 1] - gpu_win_offsets[i];
-            int w = wins-1;
-            std::cout << "\nflat_start: " << std::to_string(((size_t)gpu_win_offsets[i] + w) * HV_DIM) << "\n";
+            
+            std::cout << "\nWE ARE GETTING HERE\n";
 
             cudaFree(d_seq_packed);
             cudaFree(d_seq_offsets_dev);

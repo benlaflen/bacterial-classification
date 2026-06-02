27efd750a9ece93bbec258004e6b684457fd0182
benlaflen <114254189+benlaflen@users.noreply.github.com>
2026-06-01 17:30:02 -0700

    correct imports

--- src/GPU.cuh
+++ src/GPU.cuh
@@ -1,6 +1,7 @@
 #pragma once
 #include "config.h"
 #include <string>
+#include <unordered_map>
 
 std::vector<std::vector<HV_16>> encode(const std::vector<int16_t>& kmer_map, const std::vector<std::string>& sequences);
 
